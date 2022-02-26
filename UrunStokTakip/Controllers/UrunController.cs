3e1262f90d1816aecd3a294fb55e15928eabbac1
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-02-27 00:10:55 +0300

    update tamamlandı

--- UrunStokTakip/Controllers/UrunController.cs
+++ UrunStokTakip/Controllers/UrunController.cs
@@ -52,5 +52,52 @@ namespace UrunStokTakip.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Guncelle(int id)
+        {
+            var guncelle = db.Urun.Where(x => x.Id == id).FirstOrDefault();
+            List<SelectListItem> deger1 = (from x in db.Kategori.ToList()
+
+                                           select new SelectListItem
+                                           {
+                                               Text = x.Ad,
+                                               Value = x.Id.ToString()
+                                           }).ToList();
+
+            ViewBag.ktgr = deger1;
+
+            return View(guncelle);
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Urun model, HttpPostedFileBase File)
+        {
+            var urun = db.Urun.Find(model.Id);
+
+            if (File == null)
+            {
+                urun.Ad = model.Ad;
+                urun.Aciklama = model.Aciklama;
+                urun.Fiyat = model.Fiyat;
+                urun.Stok = model.Stok;
+                urun.Populer = model.Populer;
+                urun.KategoriId = model.KategoriId;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                urun.Resim = File.FileName.ToString();
+                urun.Ad = model.Ad;
+                urun.Aciklama = model.Aciklama;
+                urun.Fiyat = model.Fiyat;
+                urun.Stok = model.Stok;
+                urun.Populer = model.Populer;
+                urun.KategoriId = model.KategoriId;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+        }
     }
 }
