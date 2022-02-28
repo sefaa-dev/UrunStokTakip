2e01531579bbfa535c9cbd173034b0f13ea07cc4
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-02-28 22:05:25 +0300

    Kategori CRUD tamam

---
+++ UrunStokTakip/Controllers/KategoriController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunStokTakip.Models;
+
+namespace UrunStokTakip.Controllers
+{
+    [Authorize(Roles = "A")]
+
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        Takip_SistemiEntities db = new Takip_SistemiEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Kategori.Where(x => x.Durum == true).ToList());
+        }
+
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Kategori data)
+        {
+            db.Kategori.Add(data);
+            data.Durum = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var kategori = db.Kategori.Where(x => x.Id == id).FirstOrDefault();
+            db.Kategori.Remove(kategori);
+            kategori.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Guncelle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Guncelle(Kategori data)
+        {
+            var guncelle = db.Kategori.Where(x => x.Id == data.Id).FirstOrDefault();
+            guncelle.Aciklama = data.Aciklama;
+            guncelle.Ad = data.Ad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
