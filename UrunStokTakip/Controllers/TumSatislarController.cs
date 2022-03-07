5c976982dd13700883debd7445759a72cc9567d8
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-03-07 23:11:53 +0300

    Tüm Satışların İstatistikleri tamam

---
+++ UrunStokTakip/Controllers/TumSatislarController.cs
@@ -0,0 +1,22 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunStokTakip.Models;
+
+namespace UrunStokTakip.Controllers
+{
+    public class TumSatislarController : Controller
+    {
+        // GET: TumSatislar
+        Takip_SistemiEntities db = new Takip_SistemiEntities();
+        [Authorize(Roles = "A")]
+
+        public ActionResult Index(int sayfa=1)
+        {
+            return View(db.Satislar.ToList().ToPagedList(sayfa,5));
+        }
+    }
+}
